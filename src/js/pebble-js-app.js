b38d1ff529245db515bdb718a3bf0bda7d5f8bd9
GGCrew <R_Connelie@hotmail.com>
2015-12-18 02:29:17 -0800

    Adding resource folder which typically contains: - menu icon and other graphics used by the application - possible "fonts" folder when using custom fonts - possible "App Store" folder for additional images used by the Pebble App Store Replaced placeholder/default data in appinfo.json with real data. Fleshed out skeleton of pebble-js-app.js to handle configuration-open and configuration-close events.

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,5 +1,36 @@
+var SERVER_NAME = "net-night.com";
+var APP_NAME = "slats";
+
+
 Pebble.addEventListener("ready",
     function(e) {
         console.log("Hello world! - Sent from your javascript application.");
     }
+);
+
+
+Pebble.addEventListener('showConfiguration', 
+	function() {
+		Pebble.openURL('http://' + SERVER_NAME + '/pebble/' + APP_NAME + '/configure.html');
+	}
+);
+
+
+Pebble.addEventListener('webviewclosed', 
+	function(e) {
+		var options = JSON.parse(decodeURIComponent(e.response));
+		var optionKeys = Object.keys(options);
+		var key;
+		var value;
+		var message = {};
+
+		for(var counter = 0; counter < optionKeys.length; counter++) {
+			key = optionKeys[counter];
+			value = options[key];
+			window.localStorage.setItem(key, value);
+			message[key] = parseInt(value);	// assumes all values are integers
+		}
+
+		Pebble.sendAppMessage(message);
+	}
 );
