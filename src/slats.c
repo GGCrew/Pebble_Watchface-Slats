732342eb39b4cfbfe1c2b90a11ae6888a0cc7bdc
GGCrew <R_Connelie@hotmail.com>
2015-12-19 19:49:00 -0800

    Haven't fixed the incorrect scaling, despite testing on CloudPebble.net.  And installing to phone causes the phone's Pebble app to crash. Entrance animation for slats working correctly!  Todo: animations when the time changes.

--- src/slats.c
+++ src/slats.c
@@ -4,8 +4,9 @@
 /**/
 
 
+#define TIME_TOP_Y 50
 #define SLAT_COUNT 64
-
+#define ANIMATION_DURATION 1000
 
 /**/
 
@@ -13,6 +14,8 @@
 static Window *window;
 
 static TextLayer *text_time_layers[SLAT_COUNT];
+
+static PropertyAnimation *slat_animations[SLAT_COUNT];
 
 
 void update_display_time(struct tm *tick_time) {
@@ -35,15 +38,38 @@ void update_display_time(struct tm *tick_time) {
     memmove(time_text, &time_text[1], sizeof(time_text) - 1);
   }
 
-	for(slat_counter = 0; slat_counter < SLAT_COUNT; slat_counter++)
-	{
+	for(slat_counter = 0; slat_counter < SLAT_COUNT; slat_counter++) {
 		text_layer_set_text(text_time_layers[slat_counter], time_text);
 	}
 }
 
 
+void animation_stopped(Animation *animation, bool finished, void *property_animation) {
+	if(finished){property_animation_destroy(property_animation);}
+}
+
+
 void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
+	int slat_counter;
+	int delay;
+	
 	update_display_time(tick_time);
+
+	for(slat_counter = 0; slat_counter < SLAT_COUNT; slat_counter++) {
+		delay = slat_counter * 100;
+		slat_animations[slat_counter] = property_animation_create_layer_frame(text_layer_get_layer(text_time_layers[slat_counter]), NULL, &GRect(0, TIME_TOP_Y+slat_counter, 144, 1));
+		animation_set_curve(property_animation_get_animation(slat_animations[slat_counter]), AnimationCurveEaseOut);
+		animation_set_delay(property_animation_get_animation(slat_animations[slat_counter]), 0 + delay);
+		animation_set_duration(property_animation_get_animation(slat_animations[slat_counter]), ANIMATION_DURATION);
+		animation_set_handlers(property_animation_get_animation(slat_animations[slat_counter]), 
+														(AnimationHandlers) {
+															.started = NULL,
+															.stopped = (AnimationStoppedHandler) animation_stopped,
+														},
+														slat_animations[slat_counter]);
+		animation_schedule(property_animation_get_animation(slat_animations[slat_counter]));
+		
+	}
 }
 
 
@@ -53,9 +79,9 @@ static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-	for(slat_counter = 0; slat_counter < SLAT_COUNT; slat_counter++)
-	{
-		text_time_layers[slat_counter] = text_layer_create(GRect(0, 50+slat_counter, 144, 1));
+	for(slat_counter = 0; slat_counter < SLAT_COUNT; slat_counter++) {
+		APP_LOG(APP_LOG_LEVEL_DEBUG, "%d %d, %d, %d", slat_counter, TIME_TOP_Y+slat_counter, -slat_counter, TIME_TOP_Y+slat_counter-slat_counter);
+		text_time_layers[slat_counter] = text_layer_create(GRect(144, TIME_TOP_Y+slat_counter, 144, 1));
 		text_layer_set_font(text_time_layers[slat_counter], fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
 		text_layer_set_text_color(text_time_layers[slat_counter], GColorWhite);
 		text_layer_set_text_alignment(text_time_layers[slat_counter], GTextAlignmentCenter);
@@ -69,8 +95,7 @@ static void window_load(Window *window) {
 static void window_unload(Window *window) {
 	int slat_counter;
 	
-	for(slat_counter = 0; slat_counter < SLAT_COUNT; slat_counter++)
-	{
+	for(slat_counter = 0; slat_counter < SLAT_COUNT; slat_counter++) {
 		text_layer_destroy(text_time_layers[slat_counter]);
 	}
 }
