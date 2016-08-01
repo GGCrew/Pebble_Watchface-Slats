5e1057bd8c711e839364c75091890a64ce2da0f3
GGCrew <R_Connelie@hotmail.com>
2016-08-01 00:32:50 -0700

    Further abstract functionality and remove some hard-coded settings from internal rendering functions

--- src/slat_object.c
+++ src/slat_object.c
@@ -15,8 +15,8 @@
 
 // Declarations for private internal functions
 
-void slat_object_update_time_bitmap(SlatObject *slat_object, TextLayer *time_text_layer, GContext *ctx);
-void slat_object_update_slat_bitmaps(SlatObject *slat_object);
+void slat_object_render_text_bitmap(SlatObject *slat_object, GContext *ctx);
+void slat_object_set_slat_bitmaps(SlatObject *slat_object);
 void slat_animation_stopped(Animation *animation, bool finished, void *property_animation);
 
 
@@ -36,11 +36,11 @@ SlatObject* slat_object_create() {
 	
 	bitmap_size = GSize(144, SLAT_COUNT);
 	bitmap_format = GBitmapFormat1Bit;	// for Aplite
-	slat_object->time_bitmap = gbitmap_create_blank(bitmap_size, bitmap_format);
+	slat_object->text_bitmap = gbitmap_create_blank(bitmap_size, bitmap_format);
 
 	for(slat_counter = 0; slat_counter < SLAT_COUNT; slat_counter++) {
 		slat_object->slat_layers[slat_counter] = bitmap_layer_create(GRect(TIME_X_ORIGIN, TIME_Y_ORIGIN + slat_counter, 144, 1));
-		slat_object->slat_bitmaps[slat_counter] = gbitmap_create_as_sub_bitmap(slat_object->time_bitmap, GRect(0, slat_counter, 144, 1));
+		slat_object->slat_bitmaps[slat_counter] = gbitmap_create_as_sub_bitmap(slat_object->text_bitmap, GRect(0, slat_counter, 144, 1));
 		bitmap_layer_set_bitmap(slat_object->slat_layers[slat_counter], slat_object->slat_bitmaps[slat_counter]);
 	}
 	
@@ -60,8 +60,8 @@ void slat_object_destroy(SlatObject *slat_object) {
 		layer_destroy(bitmap_layer_get_layer(slat_object->slat_layers[slat_counter]));
 	}
 
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "gbitmap_destroy(slat_object->time_bitmap)...");
-	gbitmap_destroy(slat_object->time_bitmap);
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "gbitmap_destroy(slat_object->text_bitmap)...");
+	gbitmap_destroy(slat_object->text_bitmap);
 
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "free(slat_object)...");
 	free(slat_object);
@@ -70,15 +70,27 @@ void slat_object_destroy(SlatObject *slat_object) {
 }
 
 
-void slat_object_update_time(SlatObject *slat_object, TextLayer *time_text_layer, GContext *ctx) {
+void slat_object_set_text(SlatObject *slat_object, const char *text) {
+	slat_object->text = text;
+}
+
+
+void slat_object_set_font(SlatObject *slat_object, GFont font) {slat_object->font = font;}
+void slat_object_set_text_alignment(SlatObject *slat_object, GTextAlignment text_alignment) {slat_object->text_alignment = text_alignment;}
+void slat_object_set_text_color(SlatObject *slat_object, GColor color) {slat_object->text_color = color;}
+void slat_object_set_background_color(SlatObject *slat_object, GColor color) {slat_object->background_color = color;}
+void slat_object_set_size(SlatObject *slat_object, GSize size) {slat_object->size = size;}
+void slat_object_set_overflow_mode(SlatObject *slat_object, GTextOverflowMode overflow_mode) {slat_object->overflow_mode = overflow_mode;}
+
+void slat_object_render(SlatObject *slat_object, GContext *ctx) {
 	GBitmap *screen_bitmap;
 	GBitmapFormat screen_bitmap_format;
 	uint8_t *screen_bitmap_data;
 	uint16_t screen_bitmap_row_size;
 
-	GBitmapFormat time_bitmap_format;
-	uint8_t *time_bitmap_data;
-	uint16_t time_bitmap_row_size;
+	GBitmapFormat text_bitmap_format;
+	uint8_t *text_bitmap_data;
+	uint16_t text_bitmap_row_size;
 
 	/**/
 
@@ -91,20 +103,20 @@ void slat_object_update_time(SlatObject *slat_object, TextLayer *time_text_layer
 	graphics_release_frame_buffer(ctx, screen_bitmap);
 
 	// Get time bitmap data
-	time_bitmap_data = gbitmap_get_data(slat_object->time_bitmap);
-	time_bitmap_format = gbitmap_get_format(slat_object->time_bitmap);
-	time_bitmap_row_size = gbitmap_get_bytes_per_row(slat_object->time_bitmap);
+	text_bitmap_data = gbitmap_get_data(slat_object->text_bitmap);
+	text_bitmap_format = gbitmap_get_format(slat_object->text_bitmap);
+	text_bitmap_row_size = gbitmap_get_bytes_per_row(slat_object->text_bitmap);
 
 	// Point the screen bitmap (aka frame buffer) to the time bitmap (so we can render "offscreen")
 	// This trick pulled from 2015 Pebble Developer Retreat presentation on Graphics by Matthew Hungerford
-	gbitmap_set_data(screen_bitmap, time_bitmap_data, time_bitmap_format, time_bitmap_row_size, false);
+	gbitmap_set_data(screen_bitmap, text_bitmap_data, text_bitmap_format, text_bitmap_row_size, false);
 
-	slat_object_update_time_bitmap(slat_object, time_text_layer, ctx);
+	slat_object_render_text_bitmap(slat_object, ctx);
 
 	// Point the screen bitmap (aka frame buffer) back to it's original data
 	gbitmap_set_data(screen_bitmap, screen_bitmap_data, screen_bitmap_format, screen_bitmap_row_size, false);
 
-	slat_object_update_slat_bitmaps(slat_object);
+	slat_object_set_slat_bitmaps(slat_object);
 }
 
 
@@ -124,7 +136,7 @@ void slat_object_animate(SlatObject *slat_object) {
 		layer_set_frame(bitmap_layer_get_layer(slat_object->slat_layers[slat_counter]), GRect(144, TIME_Y_ORIGIN + slat_counter, 144, 1));
 
 		// Animate slats
-		start_position_x_offset = (((slat_counter & 1) == 1) ? 144 : -144);  // Zipper effect
+		start_position_x_offset = (((slat_counter & 1) == 1) ? 144 : -144);  // Interleave effect
 		start_position = GRect(start_position_x_offset, TIME_Y_ORIGIN + slat_counter, 144, 1);
 		stop_position = GRect(TIME_X_ORIGIN, TIME_Y_ORIGIN + slat_counter, 144, 1);
 		
@@ -166,40 +178,28 @@ void layer_add_slat_object(Layer *layer, SlatObject *slat_object) {
 
 // Private internal functions declared at top of this file
 
-void slat_object_update_time_bitmap(SlatObject *slat_object, TextLayer *time_text_layer, GContext *ctx) {
-	const char *time_text;
-	GFont time_font;
-	GRect time_rect = {{0, 0}, {144, SLAT_COUNT}};
-
-	uint8_t text_color_ARGB8;
-	uint8_t background_color_ARGB8;
+void slat_object_render_text_bitmap(SlatObject *slat_object, GContext *ctx) {
+	GRect text_rect = {{0, 0}, slat_object->size};
 
 	/**/
 
-	// Get text data
-	time_text = text_layer_get_text(time_text_layer);
-	//time_font = text_layer_get_font(time_text_layer);  <--- Sure wish this function existed!
-	time_font = fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49);	// This should be defined elsewhere
-
 	// Set background
-	background_color_ARGB8 = GColorBlackARGB8;
-	graphics_context_set_fill_color(ctx, (GColor8){.argb=background_color_ARGB8});
-	graphics_fill_rect(ctx, time_rect, 0, GCornerNone);
+	graphics_context_set_fill_color(ctx, slat_object->background_color);
+	graphics_fill_rect(ctx, text_rect, 0, GCornerNone);
 
 	// Render text
-	text_color_ARGB8 = GColorWhiteARGB8;
-	graphics_context_set_text_color(ctx, (GColor8){.argb=text_color_ARGB8});
+	graphics_context_set_text_color(ctx, slat_object->text_color);
 	graphics_draw_text(ctx, 
-											time_text,
-											time_font,
-											time_rect,
-											GTextOverflowModeWordWrap,
-											GTextAlignmentCenter,
+											slat_object->text,
+											slat_object->font,
+											text_rect,
+											slat_object->overflow_mode,
+											slat_object->text_alignment,
 											NULL);
 }
 
 
-void slat_object_update_slat_bitmaps(SlatObject *slat_object) {
+void slat_object_set_slat_bitmaps(SlatObject *slat_object) {
 	int slat_counter;
 
 	/**/
@@ -207,7 +207,7 @@ void slat_object_update_slat_bitmaps(SlatObject *slat_object) {
 	for(slat_counter = 0; slat_counter < SLAT_COUNT; slat_counter++) {
 		// Could use memcpy() here instead of destroy/create ???
 		gbitmap_destroy(slat_object->slat_bitmaps[slat_counter]);
-		slat_object->slat_bitmaps[slat_counter] = gbitmap_create_as_sub_bitmap(slat_object->time_bitmap, GRect(0, slat_counter, 144, 1));
+		slat_object->slat_bitmaps[slat_counter] = gbitmap_create_as_sub_bitmap(slat_object->text_bitmap, GRect(0, slat_counter, 144, 1));
 		bitmap_layer_set_bitmap(slat_object->slat_layers[slat_counter], slat_object->slat_bitmaps[slat_counter]);
 	}
 }
--- src/slat_object.h
+++ src/slat_object.h
@@ -11,10 +11,17 @@
 
 
 typedef struct {
-	GBitmap *time_bitmap;
+	GBitmap *text_bitmap;
 	GBitmap *slat_bitmaps[SLAT_COUNT];
 	BitmapLayer *slat_layers[SLAT_COUNT];
 	PropertyAnimation *slat_animations[SLAT_COUNT];
+	const char *text;
+	GFont font;
+	GTextAlignment text_alignment;
+	GColor text_color;
+	GColor background_color;
+	GSize size;
+	GTextOverflowMode overflow_mode;
 } SlatObject;
 
 
@@ -24,7 +31,14 @@ typedef struct {
 SlatObject* slat_object_create();
 
 void slat_object_destroy(SlatObject *slat_object);
-void slat_object_update_time(SlatObject *slat_object, TextLayer *time_text_layer, GContext *ctx);
+void slat_object_set_text(SlatObject *slat_object, const char *text);
+void slat_object_set_font(SlatObject *slat_object, GFont font);
+void slat_object_set_text_alignment(SlatObject *slat_object, GTextAlignment text_alignment);
+void slat_object_set_text_color(SlatObject *slat_object, GColor color);
+void slat_object_set_background_color(SlatObject *slat_object, GColor color);
+void slat_object_set_size(SlatObject *slat_object, GSize size);
+void slat_object_set_overflow_mode(SlatObject *slat_object, GTextOverflowMode overflow_mode);
+void slat_object_render(SlatObject *slat_object, GContext *ctx);
 void slat_object_animate(SlatObject *slat_object);
 
 void layer_add_slat_object(Layer *layer, SlatObject *slat_object);
